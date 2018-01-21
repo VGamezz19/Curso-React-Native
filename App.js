6799ef3c2e5c446e78fe4ab916d1b443dd2d89d8
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-21 22:48:38 +0100

    Class 13

--- App.js
+++ App.js
@@ -10,31 +10,27 @@ import {
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
+import ArtistBox from './ArtistBox'
+
 export default class PlatziMusic extends Component {
   render() {
-    const image = 'https://lastfm-img2.akamaized.net/i/u/300x300/31a51f6e3ec647c8997150ec837891c7.png'
-    const name = 'David Bowie'
-    const likes = 200
-    const comments = 140
-
+    const artist = {
+      image: 'https://lastfm-img2.akamaized.net/i/u/300x300/31a51f6e3ec647c8997150ec837891c7.png',
+      name: 'David Bowie',
+      likes:200,
+      comments:  140
+    }
     return (
       <View style={styles.container}>
-        <View style={styles.artistBox}>
-          <Image style={styles.image} source={{ uri: image }} />
-          <View style={styles.info}>
-            <Text style={styles.name}>{name}</Text>
-            <View style={styles.row}>
-              <View style={styles.iconContainer}>
-                <Icon name="ios-heart-outline" size={30} color="gray" />
-                <Text style={styles.count}>{likes}</Text>
-              </View>
-              <View style={styles.iconContainer}>
-                <Icon name="ios-chatboxes-outline" size={30} color="gray" />
-                <Text style={styles.count}>{comments}</Text>
-              </View>
-            </View>
-          </View>
-        </View>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
+        <ArtistBox artist = {artist}/>
       </View>
     );
   }
